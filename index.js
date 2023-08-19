86419a519db873532ab331d87c7dbe25d1059837
pawankumarpawan <127922273+pawankumarpawan@users.noreply.github.com>
2023-08-20 00:43:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for(i=0;i<numberOfDrumButtons;i++){
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
     var buttonInnerHTML=this.innerHTML;
   makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
 });
 
 }
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -39,3 +41,14 @@ function makeSound(key){
             console.log(buttonInnerHTML);
     }
 }
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+  
+    activeButton.classList.add("pressed");
+  
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+  
+  }
